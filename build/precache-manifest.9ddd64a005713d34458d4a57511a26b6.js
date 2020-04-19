59d4bb21fa03e332fdf55297cfc19bdaac064d4d
Henderson Pinto <hendersoncode@gmail.com>
2020-04-19 15:04:43 +0200

    Initial dist subtree commit

---
+++ build/precache-manifest.9ddd64a005713d34458d4a57511a26b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75af8d7ae350955e2af87019cae9c914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3f3f23f8f08b3b39a9",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fd9c89cd8e222550de42",
+    "url": "/static/css/main.a3172905.chunk.css"
+  },
+  {
+    "revision": "fe3f3f23f8f08b3b39a9",
+    "url": "/static/js/2.5f38f569.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5f38f569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9c89cd8e222550de42",
+    "url": "/static/js/main.13682e11.chunk.js"
+  },
+  {
+    "revision": "0d7999d210d580045d80",
+    "url": "/static/js/runtime-main.815034b7.js"
+  },
+  {
+    "revision": "2f75e7676f9dcee2856026acb080d3c8",
+    "url": "/static/media/bag.2f75e767.svg"
+  },
+  {
+    "revision": "bb41e84344ed08bcac5cfe06a1bfa901",
+    "url": "/static/media/herovideo.bb41e843.mp4"
+  },
+  {
+    "revision": "ce99fde8f6db5e5f9fdf78b515dbd9b3",
+    "url": "/static/media/logo.ce99fde8.svg"
+  }
+]);
